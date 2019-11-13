e7be45c2e8465d79f3977e8478a1a091cc4753ce
Barni <33651659+barna21@users.noreply.github.com>
2019-11-13 13:01:16 +0200

    version 1.02

---
+++ WinRar/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WinRar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtBoxLocationLoad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLoadFolders = new System.Windows.Forms.Button();
+            this.listViewFolders = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPacientiGasiti = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxArhivePac = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxDeleteFolder = new System.Windows.Forms.CheckBox();
+            this.btnArhiveLoop = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblArhivatiCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(718, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dezarhivare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(830, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Arhivare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtBoxLocationLoad
+            // 
+            this.txtBoxLocationLoad.Location = new System.Drawing.Point(96, 15);
+            this.txtBoxLocationLoad.Name = "txtBoxLocationLoad";
+            this.txtBoxLocationLoad.Size = new System.Drawing.Size(775, 20);
+            this.txtBoxLocationLoad.TabIndex = 2;
+            this.txtBoxLocationLoad.Text = "\\\\10.200.10.65\\Archive\\XVI\\2014-12-06";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folder cautare:";
+            // 
+            // btnLoadFolders
+            // 
+            this.btnLoadFolders.Location = new System.Drawing.Point(877, 13);
+            this.btnLoadFolders.Name = "btnLoadFolders";
+            this.btnLoadFolders.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFolders.TabIndex = 4;
+            this.btnLoadFolders.Text = "Load folders";
+            this.btnLoadFolders.UseVisualStyleBackColor = true;
+            this.btnLoadFolders.Click += new System.EventHandler(this.btnLoadFolders_Click);
+            // 
+            // listViewFolders
+            // 
+            this.listViewFolders.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewFolders.HideSelection = false;
+            this.listViewFolders.Location = new System.Drawing.Point(12, 41);
+            this.listViewFolders.Name = "listViewFolders";
+            this.listViewFolders.Size = new System.Drawing.Size(678, 348);
+            this.listViewFolders.TabIndex = 5;
+            this.listViewFolders.UseCompatibleStateImageBehavior = false;
+            this.listViewFolders.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nume folder:";
+            this.columnHeader1.Width = 137;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Path";
+            this.columnHeader2.Width = 776;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(696, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Pacienti gasiti:";
+            // 
+            // lblPacientiGasiti
+            // 
+            this.lblPacientiGasiti.AutoSize = true;
+            this.lblPacientiGasiti.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPacientiGasiti.Location = new System.Drawing.Point(787, 56);
+            this.lblPacientiGasiti.Name = "lblPacientiGasiti";
+            this.lblPacientiGasiti.Size = new System.Drawing.Size(16, 16);
+            this.lblPacientiGasiti.TabIndex = 7;
+            this.lblPacientiGasiti.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(692, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Arhiveaza primii:";
+            // 
+            // txtBoxArhivePac
+            // 
+            this.txtBoxArhivePac.Location = new System.Drawing.Point(781, 158);
+            this.txtBoxArhivePac.Name = "txtBoxArhivePac";
+            this.txtBoxArhivePac.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxArhivePac.TabIndex = 9;
+            this.txtBoxArhivePac.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(887, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "pacienti";
+            // 
+            // checkBoxDeleteFolder
+            // 
+            this.checkBoxDeleteFolder.AutoSize = true;
+            this.checkBoxDeleteFolder.Location = new System.Drawing.Point(695, 184);
+            this.checkBoxDeleteFolder.Name = "checkBoxDeleteFolder";
+            this.checkBoxDeleteFolder.Size = new System.Drawing.Size(162, 17);
+            this.checkBoxDeleteFolder.TabIndex = 11;
+            this.checkBoxDeleteFolder.Text = "Dupa arhivare sterge folderul";
+            this.checkBoxDeleteFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnArhiveLoop
+            // 
+            this.btnArhiveLoop.Location = new System.Drawing.Point(830, 207);
+            this.btnArhiveLoop.Name = "btnArhiveLoop";
+            this.btnArhiveLoop.Size = new System.Drawing.Size(101, 23);
+            this.btnArhiveLoop.TabIndex = 12;
+            this.btnArhiveLoop.Text = "Arhivare LISTA";
+            this.btnArhiveLoop.UseVisualStyleBackColor = true;
+            this.btnArhiveLoop.Click += new System.EventHandler(this.btnArhiveLoop_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 395);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(940, 18);
+            this.progressBar.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(696, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Pacienti arhivati:";
+            // 
+            // lblArhivatiCount
+            // 
+            this.lblArhivatiCount.AutoSize = true;
+            this.lblArhivatiCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArhivatiCount.Location = new System.Drawing.Point(787, 88);
+            this.lblArhivatiCount.Name = "lblArhivatiCount";
+            this.lblArhivatiCount.Size = new System.Drawing.Size(16, 16);
+            this.lblArhivatiCount.TabIndex = 15;
+            this.lblArhivatiCount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 420);
+            this.Controls.Add(this.lblArhivatiCount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnArhiveLoop);
+            this.Controls.Add(this.checkBoxDeleteFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxArhivePac);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPacientiGasiti);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listViewFolders);
+            this.Controls.Add(this.btnLoadFolders);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxLocationLoad);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arhivare XVI - v. 1.02";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtBoxLocationLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLoadFolders;
+        private System.Windows.Forms.ListView listViewFolders;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPacientiGasiti;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxArhivePac;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxDeleteFolder;
+        private System.Windows.Forms.Button btnArhiveLoop;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblArhivatiCount;
+    }
+}
+
---
+++ WinRar/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WinRar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Thread Thread_ARHIVARE;
+
+        public string FolderDezArhivat = @"C:\Users\Barni\Downloads\03Toamna.rar";
+        public string DeArhivat = @"C:\Users\Barni\Downloads\03Toamna\";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string folder = Application.StartupPath + @"Xfolder";
+            System.Diagnostics.Process P_WinRar = new System.Diagnostics.Process();
+            P_WinRar.StartInfo.FileName = @"C:\Program Files\WinRAR\WinRAR.exe";
+            P_WinRar.StartInfo.CreateNoWindow = false;
+            P_WinRar.StartInfo.Arguments = string.Format("x -o+ \"{0}\" \"{1}\"", FolderDezArhivat, @"C:\Users\Barni\Desktop\GitHub\WinRar\WinRar\bin\Debug\Xfolder\");
+            P_WinRar.EnableRaisingEvents = true;
+            P_WinRar.Start();
+            //MessageBox.Show("Done");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string targetArchiveName = @"archive.rar",
+            targetFile = @"C:\Users\Barni\Downloads\ArhivaTest\patient_AMS 4034";
+            ProcessStartInfo startInfo = new ProcessStartInfo("WinRAR.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            startInfo.Arguments = string.Format("a -m5 \"{0}\" \"{1}\"",
+                                  targetArchiveName, targetFile);
+            try
+            {
+                // Start the process with the info we specified.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+
+
+                }
+            }
+            catch
+            {
+                {
+                    MessageBox.Show("Error Open");
+                }
+            }
+
+            MessageBox.Show("Done");
+        }
+
+        private void btnLoadFolders_Click(object sender, EventArgs e)
+        {
+            listViewFolders.Items.Clear();
+            
+            var directories = Directory.GetDirectories(txtBoxLocationLoad.Text);
+
+            for(int z = 0; z < directories.Length; z++)
+            {
+
+                string lastFolderName = Path.GetFileName(directories[z]);
+
+                ListViewItem item = new ListViewItem(lastFolderName);
+                item.SubItems.Add(directories[z]);
+                listViewFolders.Items.Add(item);
+            }
+
+            lblPacientiGasiti.Text = listViewFolders.Items.Count.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listViewFolders.FullRowSelect = true;
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnArhiveLoop_Click(object sender, EventArgs e)
+        {
+            Thread_ARHIVARE = new Thread(arhivareThread);
+            Thread_ARHIVARE.Start();
+
+            //arhivareThread();
+        }
+
+        public void arhivareThread()
+        {
+            checkBoxDeleteFolder.Enabled = false;
+
+            if(txtBoxArhivePac.Text == "0")
+            {
+                LimitatOrNot = 0;
+                progressBar.Maximum = listViewFolders.Items.Count;
+                ArhivarePropriuzisa();
+            }
+            else
+            {
+                LimitatOrNot = 1;
+                progressBar.Maximum = Convert.ToInt32(txtBoxArhivePac.Text);
+                ArhivarePropriuzisa();
+            }
+
+            checkBoxDeleteFolder.Enabled = true;
+        }
+
+        public int LimitatOrNot;
+
+        public void ArhivarePropriuzisa()
+        {
+            if (LimitatOrNot == 0)
+            {
+                for (int x = 0; x < listViewFolders.Items.Count; x++)
+                {
+                    listViewFolders.SelectedItems.Clear();
+
+                    progressBar.Value = x + 1;
+                    lblArhivatiCount.Text = Convert.ToString(x + 1);
+
+                    this.Focus();
+                    listViewFolders.Focus();
+
+                    listViewFolders.Items[x].Selected = true;
+                    listViewFolders.Items[x].Focused = true;
+                    listViewFolders.Select();
+                    listViewFolders.EnsureVisible(x);
+
+                    string targetArchiveName = txtBoxLocationLoad.Text + "/" + listViewFolders.Items[x].SubItems[0].Text + ".rar",
+                    targetFile = listViewFolders.Items[x].SubItems[1].Text;
+                    ProcessStartInfo startInfo = new ProcessStartInfo("WinRAR.exe");
+                    startInfo.WindowStyle = ProcessWindowStyle.Maximized;
+                    startInfo.Arguments = string.Format("a -m5 \"{0}\" \"{1}\"",
+                                          targetArchiveName, targetFile);
+                    try
+                    {
+                        // Start the process with the info we specified.
+                        using (Process exeProcess = Process.Start(startInfo))
+                        {
+                            exeProcess.WaitForExit();
+                        }
+                    }
+                    catch
+                    {
+                        {
+                            MessageBox.Show("Error Open");
+                        }
+                    }
+
+                    if (checkBoxDeleteFolder.Checked == true)
+                    {
+                        if (File.Exists(txtBoxLocationLoad.Text + "/" + listViewFolders.Items[x].SubItems[0].Text + ".rar"))
+                        {
+                            try
+                            {
+                                Directory.Delete(txtBoxLocationLoad.Text + "/" + listViewFolders.Items[x].SubItems[0].Text, true);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int x = 0; x < Convert.ToInt32(txtBoxArhivePac.Text); x++)
+                {
+                    listViewFolders.SelectedItems.Clear();
+
+                    progressBar.Value = x + 1;
+                    lblArhivatiCount.Text = Convert.ToString(x + 1);
+
+                    this.Focus();
+                    listViewFolders.Focus();
+
+                    listViewFolders.Items[x].Selected = true;
+                    listViewFolders.Items[x].Focused = true;
+                    listViewFolders.Select();
+                    listViewFolders.EnsureVisible(x);
+
+                    string targetArchiveName = txtBoxLocationLoad.Text + "/" + listViewFolders.Items[x].SubItems[0].Text + ".rar",
+                    targetFile = listViewFolders.Items[x].SubItems[1].Text;
+                    ProcessStartInfo startInfo = new ProcessStartInfo("WinRAR.exe");
+                    startInfo.WindowStyle = ProcessWindowStyle.Maximized;
+                    startInfo.Arguments = string.Format("a -m5 \"{0}\" \"{1}\"",
+                                          targetArchiveName, targetFile);
+                    try
+                    {
+                        // Start the process with the info we specified.
+                        using (Process exeProcess = Process.Start(startInfo))
+                        {
+                            exeProcess.WaitForExit();
+                        }
+                    }
+                    catch
+                    {
+                        {
+                            MessageBox.Show("Error Open");
+                        }
+                    }
+
+                    if (checkBoxDeleteFolder.Checked == true)
+                    {
+                        if (File.Exists(txtBoxLocationLoad.Text + "/" + listViewFolders.Items[x].SubItems[0].Text + ".rar"))
+                        {
+                            try
+                            {
+                                Directory.Delete(txtBoxLocationLoad.Text + "/" + listViewFolders.Items[x].SubItems[0].Text, true);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show("Arhivarea s-a terminat cu succes", "Arhivare completa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            progressBar.Value = 0;
+            checkBoxDeleteFolder.Enabled = true;
+        }
+    }
+}
